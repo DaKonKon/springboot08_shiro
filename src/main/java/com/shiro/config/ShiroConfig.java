5d2da2d542baca302454b73c1a19e511257d8a2c
Caojs <1041499672@qq.com>
2021-01-21 23:38:41 +0800

    搭建测试基本框架，学习shiro的认证

--- src/main/java/com/shiro/config/ShiroConfig.java
+++ src/main/java/com/shiro/config/ShiroConfig.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 
 @Configuration
@@ -20,7 +23,7 @@ public class ShiroConfig {
     @Bean(name="securityManager")
     public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("userRealm") UserRealm userRealm){
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        securityManager.setRealms((Collection<Realm>) userRealm);
+        securityManager.setRealms(Collections.singleton(userRealm));
         return securityManager;
     }
 
@@ -29,6 +32,21 @@ public class ShiroConfig {
         ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
         //设置安全管理器
         bean.setSecurityManager(securityManager);
+
+        //添加shiro的内置过滤器
+        /*
+            anno:无需认证即可访问
+            authc:必须认证才能访问
+            user:必须拥有记住我功能才能访问
+            perms:必须拥有对某个资源的权限才能访问
+            role:必须拥有某个角色权限才能访问
+         */
+        Map<String,String> filterMap = new LinkedHashMap<>();
+        filterMap.put("/user/add","authc");
+        filterMap.put("/user/update","authc");
+        bean.setFilterChainDefinitionMap(filterMap);
+        //设置登录路由
+        bean.setLoginUrl("/toLogin");
         return bean;
     }
 
--- src/main/java/com/shiro/config/UserRealm.java
+++ src/main/java/com/shiro/config/UserRealm.java
@@ -1,11 +1,11 @@
 package com.shiro.config;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
 
 //自定义的realm extends AuthorizingRealm
 public class UserRealm extends AuthorizingRealm {
@@ -19,8 +19,16 @@ public class UserRealm extends AuthorizingRealm {
 
     //认证
     @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
         System.out.println("执行了认证");
-        return null;
+        //用户名、密码 数据库取
+        String name="root";
+        String password = "123";
+        UsernamePasswordToken userToken = (UsernamePasswordToken) token;
+        if(!userToken.getUsername().equals(name)){
+            return null;//自动抛出异常 UnknownAccountException
+        }
+        //密码认证shiro做
+        return new SimpleAuthenticationInfo("",password,"");
     }
 }
